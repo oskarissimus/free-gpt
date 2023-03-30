cd9ee5dc2aceb9d522fb7d176e62cf83bf1e76b5
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-30 06:01:32 +0200

    refactor: Add type annotations and token counting function to utils.py

--- mercury/mercury/utils.py
+++ mercury/mercury/utils.py
@@ -1,3 +1,5 @@
+import tiktoken
+from typing import Optional
 import re
 
 
@@ -5,3 +7,8 @@ def extract_code(response_text):
     code_pattern = re.compile(r"```\n(.*?)```", re.DOTALL)
     code_matches = code_pattern.findall(response_text)
     return code_matches
+
+
+def count_tokens(text: str) -> Optional[int]:
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    return len(encoding.encode(text))
