17a28564d6a00356cbd14d68617e775ecd7c3f8a
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-30 15:14:27 +0200

    feat: Add utility functions for text processing and AI.

--- mercury/mercury/utils.py
+++ mercury/mercury/utils.py
@@ -1,6 +1,7 @@
 import tiktoken
 from typing import Optional
 import re
+import random
 
 
 def extract_code(response_text):
@@ -12,3 +13,42 @@ def extract_code(response_text):
 def count_tokens(text: str) -> Optional[int]:
     encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
     return len(encoding.encode(text))
+
+
+def omit_lines(text: str) -> str:
+    lines = text.splitlines()
+    top = lines[:5]
+    bottom = lines[-5:]
+    omitted_count = len(lines) - 10
+    omitted = [f"(*** {omitted_count} lines omitted ***)"]
+    return "\n".join(top + omitted + bottom)
+
+
+def trim_all_lines(text: str):
+    lines = text.splitlines()
+    return "\n".join([line.strip() for line in lines])
+
+
+def remove_random_tokens_from_line(text, keep=0.9):
+    encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    tokens = encoding.encode(text)
+    tokens_to_keep = int(len(tokens) * keep)
+    indexes_to_keep = random.sample(range(len(tokens)), tokens_to_keep)
+    indexes_to_keep.sort()
+    return encoding.decode([tokens[i] for i in indexes_to_keep])
+
+
+def remove_random_tokens_by_percent(text, keep=0.9):
+    lines = text.splitlines()
+    return "\n".join([remove_random_tokens_from_line(line, keep) for line in lines])
+
+
+def remove_random_tokens_by_count(text, keep=3000):
+    keep_percent = min(1, keep / count_tokens(text))
+    return remove_random_tokens_by_percent(text, keep_percent)
+
+
+def digest_output_for_openai(command: str, output: str, summarize):
+    output_with_ommited_lines = omit_lines(output)
+    summary = summarize(command, output)
+    return f"{output_with_ommited_lines}\n===> summary: {summary}"
