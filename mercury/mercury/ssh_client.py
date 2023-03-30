b725fa52dc549e739e96ea9d3cb320a267a5ea74
Oskar Korczak <oskar.korczak@protonmail.com>
2023-03-30 15:15:09 +0200

    refactor: Refactor SSH client for improved code structure and readability

--- mercury/mercury/ssh_client.py
+++ mercury/mercury/ssh_client.py
@@ -2,6 +2,8 @@ import paramiko
 from io import StringIO
 from mercury.settings import Settings
 import logging
+from mercury.dto import CodeExecutionDTO
+
 
 logger = logging.getLogger(__name__)
 
@@ -9,51 +11,31 @@ logger = logging.getLogger(__name__)
 class SshClient:
     def __init__(self, settings: Settings):
         self.settings = settings
-
-    @property
-    def instance_ip(self) -> str:
-        return self.settings.instance_ip
-
-    @property
-    def private_key_content(self) -> str:
-        return self.settings.private_key_content
-
-    @property
-    def username(self) -> str:
-        return self.settings.instance_username
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     @property
     def private_key(self) -> paramiko.RSAKey:
-        return paramiko.RSAKey.from_private_key(StringIO(self.private_key_content))
+        return paramiko.RSAKey.from_private_key(
+            StringIO(self.settings.private_key_content)
+        )
 
-    def execute_code(self, code: str) -> tuple[str, str]:
-        logger.info(f"Executing code: {code}")
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+    def execute_code(self, code: str) -> CodeExecutionDTO:
         try:
-            logger.info("Connecting to instance")
-            self.connect_to_instance(ssh)
-            logger.info("Connected to instance successfully")
-            logger.info("Running command")
-            output, error_output = self.run_command(ssh, code)
-            logger.info("Command executed successfully")
-            return output, error_output
+            self.connect_to_instance()
+            return self.run_command(self.ssh, code)
         except Exception as e:
             logger.error(f"Error executing code: {e}")
         finally:
-            ssh.close()
+            self.ssh.close()
 
-    def connect_to_instance(self, ssh: paramiko.SSHClient) -> None:
-        logger.info(f"Connecting to instance: {self.instance_ip}")
-        ssh.connect(self.instance_ip, username=self.username, pkey=self.private_key)
-        logger.info("Connected to instance successfully.")
+    def connect_to_instance(self) -> None:
+        self.ssh.connect(
+            self.settings.instance_ip,
+            username=self.settings.instance_username,
+            pkey=self.private_key,
+        )
 
-    def run_command(self, ssh: paramiko.SSHClient, code: str) -> tuple[str, str]:
-        logger.info(f"Running command: {code}")
-        stdin, stdout, stderr = ssh.exec_command(code)
-        output = stdout.read().decode("utf-8")
-        error_output = stderr.read().decode("utf-8")
-        logger.info(f"Command output: {output}")
-        logger.info(f"Command error output: {error_output}")
-        return output, error_output
+    def run_command(self, code: str) -> CodeExecutionDTO:
+        channels_tuple = self.ssh.exec_command(code)
+        return CodeExecutionDTO.from_channels_tuple_and_code(channels_tuple, code)
